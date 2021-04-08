d5c10a6b8c8c26fc70e143d2c2112546bd444a0e
jswz72 <jacobdasword@gmail.com>
2021-04-07 20:13:11 -0400

    Add firerate mechanic

--- sdlgame_win/src/ECS/MouseController.cpp
+++ sdlgame_win/src/ECS/MouseController.cpp
@@ -8,11 +8,12 @@
 
 void MouseController::update() {
 	auto lmb = Game::mouseButtonHandler.mouseButtonData(SDL_BUTTON_LEFT);
-	if (lmb->state) {
+	if (lmb->state && (SDL_GetTicks() - lastFire >= fireInterval)) {
 		auto collider = &entity->getComponent<ColliderComponent>().collider;
 		Vector2D colliderPos{ static_cast<float>(collider->x), static_cast<float>(collider->y) };
 		Vector2D mousePos{ Game::camera.x + lmb->pos.x, Game::camera.y + lmb->pos.y };
 		Vector2D direction = Utils::directionBetween(colliderPos, mousePos);
-		Game::assets->createProjectile(colliderPos, direction, 1000, 1, "projectile", entity);
+		Game::assets->createProjectile(colliderPos, direction, 1000, 10, "projectile", entity);
+		lastFire = SDL_GetTicks();
 	}
 }
